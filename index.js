0ce4476428aeb2e2f957352b0d964119b9f3011c
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2022-05-02 12:57:00 -0500

    solution code

--- index.js
+++ index.js
@@ -14,18 +14,37 @@ let ride4PowerElement = document.querySelector("#ride-4-power")
 //Ride 1 works when:
     // green wire is connected
 let ride1PowerOn = false
-
+if (greenWireConnected){
+    ride1PowerOn = true
+}
 //Ride 2 works when 
     //green and yellow wires are connected
 let ride2PowerOn = false
+if (greenWireConnected && yellowWireConnected){
+    ride2PowerOn = true;
+}
 
 //Ride 3 works when:
     // green and red wires are connected
     // water is on 
     // yellow wire is not connected
 let ride3PowerOn = false
+if(greenWireConnected && redWireConnected && waterIsOn && !yellowWireConnected){
+    ride3PowerOn = true
+}
 
+//Ride 4 works when
+    //red wire is connected
 let ride4PowerOn = false
+
+//Ride 4 is a slot machine
+    //if a 1 is rolled it should return "star"
+    //if a 2 is rolled it should return "car"
+    //if a 3 is rolled it should return "pot of gold"
+    //if a 4 is rolled it should return "clover"
+    //if a 5 is rolled it should return "bowling ball"
+    //if a 6 is rolled it should return "tiger"
+    //if a 7 is rolled it should return "seven"
 function ride4SlotMachine(rolledNumber){
     return "nothing"
 }
